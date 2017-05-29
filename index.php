2532d54faed43a736c03bfed848bb103c519d695
Jorge Pabon <pianistapr@hotmail.com>
2017-05-28 20:39:59 -0400

    try

--- index.php
+++ index.php
@@ -9,15 +9,7 @@ include 'includes/head.html';
 
 <body>
 
-	<div>
-		<img src="img/Logo.png">
-		<br>
-		<h3>
-		This is the index page of the application.
-		<br>
-		<button onclick="goOffline()">Take me to the offline page.</button>
-		</h3>
-	</div>
+
 
 </body>
 
