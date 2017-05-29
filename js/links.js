546c6274b9276a836dc808a307757d34a450289e
Jorge Pabon <pianistapr@hotmail.com>
2017-05-28 21:04:27 -0400

    try

--- js/links.js
+++ js/links.js
@@ -1,7 +1,5 @@
 function goHome()
 {
-    //location.href = "#home";
-	
 	var hide = document.getElementById("offline");
 	hide.style.display = 'none';
 	
@@ -11,11 +9,18 @@ function goHome()
 
 function goOffline()
 {
-    //location.href = "#offline";
-	
 	var hide = document.getElementById("home");
 	hide.style.display = 'none';
 	
 	var hide = document.getElementById("offline");
+	hide.style.display = 'block';
+}
+
+function load()
+{
+	var hide = document.getElementById("offline");
+	hide.style.display = 'none';
+	
+	var hide = document.getElementById("home");
 	hide.style.display = 'block';
 }
