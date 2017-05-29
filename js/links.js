0336f2763ba957c04dcdee9738722b8c38b37a6f
Jorge Pabon <pianistapr@hotmail.com>
2017-05-29 01:10:54 -0400

    try

--- js/links.js
+++ js/links.js
@@ -3,8 +3,8 @@ function goHome()
 	var hide = document.getElementById("offline");
 	hide.style.display = 'none';
 	
-	var hide = document.getElementById("home");
-	hide.style.display = 'block';
+	var show = document.getElementById("home");
+	show.style.display = 'block';
 }
 
 function goOffline()
@@ -12,27 +12,27 @@ function goOffline()
 	var hide = document.getElementById("home");
 	hide.style.display = 'none';
 	
-	var hide = document.getElementById("offline");
-	hide.style.display = 'block';
+	var show = document.getElementById("offline");
+	show.style.display = 'block';
 }
 
 function load()
 {
-	if(getAnchor() == "offline")
+	if(areWeOnline())
 	{
-		var hide = document.getElementById("home");
-		hide.style.display = 'none';
+		var show = document.getElementById("home");
+		show.style.display = 'block';
 		
 		var hide = document.getElementById("offline");
-		hide.style.display = 'block';
+		hide.style.display = 'none';
 	}
 	else
 	{
-		var hide = document.getElementById("offline");
-		hide.style.display = 'none';
+		var show = document.getElementById("offline");
+		show.style.display = 'block';
 		
 		var hide = document.getElementById("home");
-		hide.style.display = 'block';
+		hide.style.display = 'none';
 	}
 }
 
@@ -42,4 +42,32 @@ function getAnchor()
 	urlParts   = currentUrl.split('#');
 
 	return (urlParts.length > 1) ? urlParts[1] : null;
+}
+
+function areWeOnline()
+{
+    var xhr = new XMLHttpRequest();
+	
+    var file = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+    var randomNum = Math.round(Math.random() * 10000);
+ 
+    xhr.open('HEAD', file + "?rand=" + randomNum, true);
+    xhr.send();
+     
+    xhr.addEventListener("readystatechange", processRequest, false);
+ 
+    function processRequest(e)
+	{
+      if (xhr.readyState == 4)
+	  {
+        if (xhr.status >= 200 && xhr.status < 304)
+		{
+          return true;
+        }
+		else
+		{
+          return false;
+        }
+      }
+    }
 }
